307b3650b84346d10ae4b00a9c51a9dea0ee029e
jose55mase <josemase55@gmail.com>
2022-03-01 13:18:51 -0500

    Desarrollo nuva hola mundo google cloud

---
+++ src/test/java/com/googlecloud/nuva/NuvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.googlecloud.nuva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
